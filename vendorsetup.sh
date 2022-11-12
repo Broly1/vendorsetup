8fdfc5d52d35e06f2d9b6c4a2f8b7acd37202246
Broly <jpegrande@gmail.com>
2022-11-12 15:52:41 -0500

    clone my own repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 # Clone/Fetch Upstream Device Dependencies
 # Device tree
 if [ ! -d "device/oneplus/sm8250-common" ]; then
-	git clone -b thirteen git@github.com:PixelExperience-Devices/device_oneplus_sm8250-common.git device/oneplus/sm8250-common
+	git clone -b thirteen git@github.com:Broly1/android_device_oneplus_sm8250-common.git device/oneplus/sm8250-common
 else
 	printf "device/oneplus/sm8250-common found!\n"
 fi
